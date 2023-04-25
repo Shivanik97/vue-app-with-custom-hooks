c7ad80d9c03699c7ba965b3cf440d4d49d927633
Shivani K <shivanik.bbi18@rvce.edu.in>
2023-04-25 12:42:52 +0530

    vue app with custom hooks

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Suspense>
+      <template #default>
+        <Users />
+      </template>
+      <template #fallback>
+        <div>Loading...</div>
+      </template>
+    </Suspense>
+  </div>
+</template>
+<script setup lang="ts">
+import Users from "./components/Users.vue";
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/hooks/users.ts
@@ -0,0 +1,29 @@
+import useApi from './api'
+import { ref } from 'vue'
+export interface Location {
+  lat: number
+  lng: number
+}
+export interface Address {
+  street: string
+  suite: string
+  city: string
+  zipcode: number
+  geo: Location
+}
+export interface User {
+  id: string
+  name: string
+  username: string
+  email: string
+  address: Address
+}
+export default async function useUserss() {
+  const { response: users, request } = useApi<User[]>('https://jsonplaceholder.typicode.com/users')
+  const loaded = ref(false)
+  if (loaded.value === false) {
+    await request()
+    loaded.value = true
+  }
+  return { users }
+}
